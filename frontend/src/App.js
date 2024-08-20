c9f40afc73def5b4c805eacc1fa96388ef64681f
Pranav <pranavmaheshwari123@gmail.com>
2024-08-20 08:02:29 +0530

    Frontend Structure Added

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>EmailAI</h3>
+        <h1>Your Communication, Optimized</h1>
+        <p>
+          Craft, personalize, and automate your email replies with our free
+          AI-powered email response generator. Designed for professional
+          correspondence, customer service, and everyday communication needs.
+        </p>
+        <button>Create a New Response</button>
+
+        <h2>Customized Replies for Every Communication Need</h2>
+        <p>
+          Email Response AI is your ultimate tool for any email scenario.
+          Whether it’s customer support, professional networking, or routine
+          correspondence, our app tailors every response to your needs.
+          Experience the convenience of:
+        </p>
+        <ul>
+          <l1>
+            AI-Powered Response Optimization Utilize AI to craft the most
+            effective replies. Our tool ensures clear, concise, and contextually
+            appropriate responses.
+          </l1>
+          <li>
+            All-in-One Email Organizer Streamline your email management with our
+            all-in-one platform. Email Response AI consolidates reply templates,
+            response history, and communication tips. Manage all your email
+            responses in one place.
+          </li>
+          <li>
+            Collaborative Response Drafting Collaborate on crafting replies with
+            team members. Our real-time feature makes group communication
+            planning effortless, ensuring consistency and coherence.
+          </li>
+        </ul>
+      </header>
+      <footer>
+        Email Response AI Transform your email communications with ease and
+        efficiency using Email Response AI. Legal Terms and Conditions Privacy
+        Policy Support Contact Us © 2024 Email Response AI. All rights reserved.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
